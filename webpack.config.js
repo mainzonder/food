96f371e1f9a10fcfaf8934a0835d52d78f83c058
mainzonder <no7au53r@gmail.com>
2021-09-01 12:46:59 +0300

    27 @babel added

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,57 @@ let path = require('path');
 module.exports = {
   mode: 'development',
   entry: './js/script.js',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + '/dist/js'
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist/js'
   },
   watch: true,
 
   devtool: 'source-map',
 
-  module: {}
+
+  module: {
+/*     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ] */
+  }
 };
+
+
+
+
+/* 
+  module: {
+    test: /\.m?js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              debug: true,
+              corejs: 3,
+              useBuiltIns: "usage"
+            }
+          ]
+        ]
+      }
+    }
+  }
+};
+ */
